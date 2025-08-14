3c1d46b069449ae9811419f31cf2f719ec112e6a
Geovane Dos Santos <geovane.1998.santos@gmail.com>
2025-08-14 19:32:28 -0300

    refactor: Container and Heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import Heading from "./components/Heading/Heading";
-import Container from "./components/Container/Container";
+import Heading from "./components/Heading";
+import Container from "./components/Container";
 import { Timer } from "lucide-react";
 import "./styles/global.css";
 import "./styles/themes.css";
